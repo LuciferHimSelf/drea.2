7c41300fed10d0da8eac95a5e191a588811206ca
LuciferHimSelf <46575361+LuciferHimSelf@users.noreply.github.com>
2019-01-12 09:35:56 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,8 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "+";
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.login(process.env.NTMzNTI2MDk4NjQ3MDU2NDE0.DxsaxQ.bIT3_I548tIAD7Ih8RQ6Uu5SPdA);// لا تغير فيها شيء
